96811e4a2a0bfa64c77efe967bc7c70e455b1b33
liuyang <liuyang@xiangrikui.com>
2015-01-28 12:35:09 +0800

    使用spring data jpa + maven开发服务端接口

---
+++ src/main/java/com/hqit/jd/dbpom/App.java
@@ -0,0 +1,13 @@
+package com.hqit.jd.dbpom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
